78197288f339fc242d57b7135b0e07244551766a
HERBOD7 <herbodabdoli@gmail.com>
2023-03-10 22:05:29 +0330

    feat: show list of tasks and categories

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,30 +2,11 @@ import React from 'react';
 import Categories from './components/Categories';
 
 const Home = () => {
-  // static categories
-  const categories = [
-    {
-      title: 'Housework',
-      id: 1,
-    },
-    {
-      title: 'Shopping',
-      id: 2,
-    },
-    {
-      title: 'Personal',
-      id: 3,
-    },
-    {
-      title: 'Work',
-      id: 4,
-    },
-  ];
   return (
     <div className="py-8 px-5 w-5/6	lg:w-3/4">
       <header className="flex text-xl font-bold">My Tasks</header>
       <div className="my-5">
-        <Categories items={categories} />
+        <Categories />
       </div>
     </div>
   );
